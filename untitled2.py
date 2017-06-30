44710d22c211d761db55af279fb8951d076a0e25
ZoranaK <kostadinovici4@gmail.com>
2017-06-30 17:02:28 +0200

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 16:28:16 2017
+
+@author: User
+"""
+
+niska = "Da li je moguće da sam i dalje niska?!"
+jedan_karakter = niska[1]
+print(jedan_karakter)
+nekoliko_karaktera = niska[0:15]
+print(nekoliko_karaktera)
+nekoliko_karaktera_s_kraja = niska[-15:-2]
+print(nekoliko_karaktera_s_kraja)
+
+
+
+
+rec="pevac"
+
+osnova=rec[0:3]
+print(osnova)
+
+sufiks=rec[-2:6]
+print(sufiks)
+
+
+
+rec="beogradski"
+
+osnova1=rec[0:7]
+print(osnova1)
+sufiks()
+
